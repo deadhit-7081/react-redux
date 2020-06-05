a75f2a047837498daa65a086e273426986b7e22b
Jai <jaikumarsingh1908@gmail.com>
2020-06-05 17:37:42 +0530

    ReactRedux Async Action

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,10 +1,11 @@
 import {createStore , applyMiddleware} from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './rootReducer'
 import logger from 'redux-logger'
 
 
 //createStore method recv a reducer fn as a parameter so that it can change the state 
 //according to the action type 
-const store = createStore(rootReducer , applyMiddleware(logger))
+const store = createStore(rootReducer ,composeWithDevTools(applyMiddleware(logger)))
 
 export default store
