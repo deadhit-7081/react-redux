83c05d3d0befcc9fda45e03c1972da0971513b9d
Jai <jaikumarsingh1908@gmail.com>
2020-06-05 20:33:44 +0530

    Line Chart

--- src/App.js
+++ src/App.js
@@ -8,20 +8,24 @@ import IceCreamContainer from './components/IceCreamContainer';
 import NewCakeContainer from './components/NewCakeContainer';
 import ItemContainer from './components/ItemContainer';
 import UserContainer from './components/UserContainer';
+import LineChat from './components/LineChar';
 
 function App() {
   return (
-    <Provider store={store}>
+    //<Provider store={store}>
       <div className="App">
         { /*<ItemContainer cake/>
         <ItemContainer/>
         <HooksCakeContainer/>
         <CakeContainer/>
         <IceCreamContainer/>
-        <NewCakeContainer/> */}
-        <UserContainer/>
+        <NewCakeContainer/> 
+        <UserContainer/>*/}
+         <div className = "chart">
+           <LineChat/>
+         </div>
       </div>
-    </Provider>
+    //</Provider>
   );
 }
 
