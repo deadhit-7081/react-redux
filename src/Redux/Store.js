93a8c71c2289648ae8f8294ba0f03214274bc595
Jai <jaikumarsingh1908@gmail.com>
2020-06-05 10:53:00 +0530

    Logger Middleware in ReactRedux

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,9 +1,10 @@
-import {createStore} from 'redux'
+import {createStore , applyMiddleware} from 'redux'
 import rootReducer from './rootReducer'
+import logger from 'redux-logger'
 
 
 //createStore method recv a reducer fn as a parameter so that it can change the state 
 //according to the action type 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer , applyMiddleware(logger))
 
 export default store
